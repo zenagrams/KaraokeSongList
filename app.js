b9258666222a49e26db022e2629b82c1dfa8ce5f
zenagrams <110210132+zenagrams@users.noreply.github.com>
2023-01-11 11:35:17 -0800

    Update app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+var titles = document.getElementsByClassName('title');
+//turn title html collection into an array and cycle through it
+Array.from(titles).forEach(function(item){
+    console.log(item);
